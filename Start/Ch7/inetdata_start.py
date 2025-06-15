6f9fad6485c50ca734a32987087ec2fe23a5a278
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2025-06-15 12:24:41 +0100

    Update inetdata_start.py

--- Start/Ch7/inetdata_start.py
+++ Start/Ch7/inetdata_start.py
@@ -1,3 +1,12 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for retrieving data from the internet
 #
+# import the library to retrieve data from the internet
+import urllib.request
+
+web_url = urllib.request.urlopen("http://www.example.com")
+print("Response code: ", web_url.getcode())
+
+data = web_url.read()
+print("Data retrieved from the web:")
+print(data)
