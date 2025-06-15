7215b74f006df56c90aa33de940bf9050bb42f22
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2025-06-15 12:24:11 +0100

    Update writefile_start.py

--- Start/Ch6/writefile_start.py
+++ Start/Ch6/writefile_start.py
@@ -4,12 +4,34 @@
 
 
 # Open a file for writing and create it if it doesn't exist
-
+sample_file = open("textfile.txt","w+")
+sample_file.write("This is a sample text file.\n")
+sample_file.close()  # close the file when done
 
 # Open the file for appending text to the end
+sample_file = open("textfile.txt", "a")
+# Append some text to the file
+sample_file.write("Appending a new line to the file.\n")
+# Close the file after writing
+sample_file.close()
 
 
 # write some lines of data to the file
-
-
+sample_file = open("textfile.txt", "a+")
+sample_file.write("This is the first line.\n")
+sample_file.write("This is the second line.\n")
+sample_file.write("This is the third line.\n")
 # close the file when done
+sample_file.close()
+
+
+# Read the file contents
+sample_file = open("textfile.txt", "r")
+file_contents = sample_file.read()
+# Print the file contents
+print("File contents:")
+print(file_contents) 
+
+
+
+
