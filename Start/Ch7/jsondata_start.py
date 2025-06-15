7939360368d323514194028056159983fdfc429a
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2025-06-15 12:25:03 +0100

    Update jsondata_start.py

--- Start/Ch7/jsondata_start.py
+++ Start/Ch7/jsondata_start.py
@@ -3,12 +3,17 @@
 #
 
 import urllib.request 
-
+import json
 
 # Open the URL and read the data
-
+url_json = urllib.request.urlopen("https://uselessfacts.jsph.pl/api/v2/facts/random")
+print("Response code: ", url_json.getcode())
 
 # Read the JSON data from the source
-
+data = url_json.read()
+print("Data retrieved from the web:")
+# print(data)
 
 # Print the content of the 'text' field
+json_data = json.loads(data)
+print(json_data["text"])
