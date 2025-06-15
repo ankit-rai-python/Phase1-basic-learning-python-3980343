1b63b78eaf953ed6eabba1683d83a6c8038cc61f
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2025-06-15 12:25:50 +0100

    Update helloworld_start.py

--- Start/Ch2/helloworld_start.py
+++ Start/Ch2/helloworld_start.py
@@ -1,3 +1,4 @@
-# LinkedIn Learning Python course by Joe Marini
-# Example file for HelloWorld
 print("Hello world")
+name = input("What is your name? ")
+print(f"Hello {name}, welcome to the Python world!")
+# This is a simple Python script that greets the user
