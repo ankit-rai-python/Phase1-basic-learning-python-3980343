ea4d2f9cf6b5b376bac7acf8e6991a888bdd472c
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2025-06-15 12:21:11 +0100

    Update dates_start.py

--- Start/Ch5/dates_start.py
+++ Start/Ch5/dates_start.py
@@ -9,16 +9,28 @@ from datetime import datetime
 
 ## DATE OBJECTS
 # Get today's date from the simple today() method from the date class
+today = date.today()
+# print out the date's components
+print("Today's date is:", today)
 
 
 # print out the date's individual components
+print("Today's year is:", today.year)
+print("Today's month is:", today.month)
+print("Today's day is:", today.day)
+
 
 
 # retrieve today's weekday (0=Monday, 6=Sunday)
-
+print("Today's weekday is:", today.weekday())
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print("Today's weekday is:", days[today.weekday()])
 
 ## DATETIME OBJECTS
 # Get today's date from the datetime class
+now = datetime.now()
+print("The current date and time is:", now)
 
 
 # Get the current time
+print("The current time is:", now.time())
