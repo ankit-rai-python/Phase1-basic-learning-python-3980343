de6f036ce4a8f0862126bff53f8d56dc52cc0e55
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2025-06-15 12:17:22 +0100

    Update modules_start.py

--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -3,11 +3,20 @@
 
 # import the math module, which contains features for working with mathematics
 
+# import math
+
+# print("The value of pi is:", math.pi)  # print the value of pi
+# print("The square root of 16 is:", math.sqrt(16))  # calculate the square root of 16
+
 
 # import a specific part of the module so you can refer to it more easily
-
+from math import pi
+print("The value of pi is:", pi)  # now you can use pi directly without math. prefix
 
 # import a module and give it a different name
+import random as r
+print(r.randint(100, 200))  # use the random module to generate a random integer between 100 and 200
+
 
 
 # the math module contains lots of pre-built functions
@@ -22,7 +31,10 @@
 # try some of the math functions for yourself here:
 
 # Use the 3rd party tabulate module to print tabulated data:
-
+# pip3 install tabulate
+# https://pypi.org/project/tabulate/
+from tabulate import tabulate
+# class tabulate contains tabulate() function to create a formatted table from data
 # Sample data
 data = [
   ["Product", "Price", "Stock"],
@@ -32,3 +44,4 @@ data = [
 ]
 
 # Create a formatted table
+print(tabulate(data, headers="firstrow", tablefmt="grid"))
